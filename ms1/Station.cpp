63c18dc84795c950d5647fe01c1b82d4ebfcd078
Long Duc Hoang <88246885+duclongbox@users.noreply.github.com>
2023-11-05 23:15:58 -0500

    Add files via upload

---
+++ ms1/Station.cpp
@@ -0,0 +1,86 @@
+/*/////////////////////////////////////////////////////////////////////////
+       milestone 1
+Full Name  : Long Duc Hoang
+Student ID#: 122702228
+Email      : dhoang19@myseneca.ca
+Section    : ZAA
+Date	   : 11/1/2023
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include"Utilities.h"
+#include "Station.h"
+
+using namespace std;
+namespace sdds {
+    size_t Station::m_widthField = 0;
+    size_t Station::id_generator = 0;
+    Station::Station(const std::string & str) {
+        Utilities util{};
+        
+        size_t next_pos = 0;
+        bool more = true;
+        id_generator++;
+        m_stationID = id_generator;
+        m_name = util.extractToken(str, next_pos, more);
+        m_nextNum = std::stoul(util.extractToken(str, next_pos, more));
+        m_noOfItem = std::stoul(util.extractToken(str, next_pos, more));
+        if (util.getFieldWidth() > m_widthField) {
+            m_widthField = util.getFieldWidth();
+        }
+        m_desc = util.extractToken(str, next_pos, more);
+    }
+
+    const std::string& Station::getItemName() const {
+        return m_name;
+    }
+
+    size_t Station::getNextSerialNumber() {
+        return m_nextNum++;
+    }
+
+    size_t Station::getQuantity() const {
+        return m_noOfItem;
+    }
+
+    void  Station::updateQuantity() {
+        if (m_noOfItem > 0) {
+            m_noOfItem--;
+        }
+    }
+
+    void Station::display(std::ostream& os, bool full) const {
+        os.setf(ios::right);
+        os.width(3);
+        os.fill('0');
+        os << m_stationID << " | ";
+        os.unsetf(ios::right);
+
+        os.setf(ios::left);
+        os.width(m_widthField);
+        os.fill(' ');
+        os << m_name << " | ";
+        os.unsetf(ios::left);
+        
+        os.setf(ios::right);
+        os.width(6);
+        os.fill('0');
+        os  << m_nextNum << " | ";
+
+        if (full) {
+            os.width(4);
+            os.fill(' ');
+            os<< m_noOfItem << " | " << m_desc;
+        }
+
+        os << std::endl;
+    }
+
+
+
+}
---
+++ ms1/Utilities.cpp
@@ -0,0 +1,69 @@
+/*/////////////////////////////////////////////////////////////////////////
+	   milestone 1
+Full Name  : Long Duc Hoang
+Student ID#: 122702228
+Email      : dhoang19@myseneca.ca
+Section    : ZAA
+Date	   : 11/1/2023
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+#include <iostream>
+#include <string>
+#include "Utilities.h"
+
+using namespace std;
+namespace sdds {
+	char Utilities::m_delimiter{};
+	void Utilities::setFieldWidth(size_t newWidth) {
+		m_widthField = newWidth;
+	}
+	size_t Utilities::getFieldWidth() const {
+		return m_widthField;
+
+	}
+	string Utilities::extractToken(const std::string& str, size_t& next_pos, bool& more) {
+		string token{};
+		more = false;
+		string remaining = str;
+		size_t found = remaining.find(m_delimiter,next_pos);
+		if (found == next_pos) {
+
+			throw "delimeter at the start position";
+		}
+		remaining.erase(0, next_pos);
+		found = remaining.find(m_delimiter);
+		
+		
+		if (found != string::npos) {
+			size_t pos1 = remaining.find_first_not_of(" ");
+			size_t pos2 = remaining.find_last_not_of(" ", found);
+			token = remaining.substr(pos1, pos2 - pos1);
+			more = true;
+			next_pos += (found + 1);
+		}
+		else {
+			size_t pos1 = remaining.find_first_not_of(" ");
+			token = remaining.substr(pos1);
+			more = false;
+		}
+		
+		if(token.length()==0){
+			more = false;
+		}
+		if (token.length() > m_widthField) {
+			m_widthField = token.length();
+		}
+		return token;
+	}
+	void Utilities::setDelimiter(char newDelimiter) {
+		m_delimiter = newDelimiter;
+	}
+	char Utilities::getDelimiter() {
+		return m_delimiter;
+	}
+
+
+}
